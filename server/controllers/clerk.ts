b027c07ad5275e44e9a085097af55e6532fb2249
ruchita <ruchitarkg05@gamil.com>
2026-04-23 02:53:41 +0530

    fix webhook email fallback

--- server/controllers/clerk.ts
+++ server/controllers/clerk.ts
@@ -11,31 +11,32 @@ const clerkWebhooks = async (req:Request, res: Response) => {
 
     //Switch Cases for differnt Events
     switch(type) { 
-        case "user.created":{ 
+        
+        case "user.created": {
             await prisma.user.create({
                 data: {
                     id: data.id,
-                    email: data?.email_addresses[0]?.email_address,
+                    email: data?.email_addresses[0]?.email_address || data?.id + "@placeholder.com",
                     name: data?.first_name + " " + data?.last_name,
                     image: data?.image_url,
                 }
             })
             break;
         } 
+
         case "user.updated": {
             await prisma.user.update({
-                where:{
+                where: {
                     id: data.id
                 },
                 data: {
-                    email: data?.email_addresses[0]?.email_address,
+                    email: data?.email_addresses[0]?.email_address || data?.id + "@placeholder.com",
                     name: data?.first_name + " " + data?.last_name,
                     image: data?.image_url,
                 }
             })
             break;
         }
-
         case "user.deleted": {
             await prisma.user.delete({
                 where:{ id: data.id } }) 
